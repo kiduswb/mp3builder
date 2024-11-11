6830dfe77709068da7bf85dcf69d17c029cebad7
Kidus <mail@kidus.ca>
2024-11-11 00:08:53 -0500

    added: download box UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header()
 {
     return (
         <section className="container">
-            <div className="row py-5">
+            <div className="row mt-5 mb-3">
                 <div className="col-lg-9 mx-auto text-center">
                     <img src="/logo.svg" alt="MP3Builder Logo" className="hero-img" />
                     <h1>MP3Builder</h1>
