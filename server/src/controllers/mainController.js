f73186feb4d5690609b15d9f2d8d2419071ad09e
Kidus <mail@kidus.ca>
2024-12-02 18:08:55 -0500

    added: user agent

--- server/src/controllers/mainController.js
+++ server/src/controllers/mainController.js
@@ -87,7 +87,8 @@ export const convertYouTubeLink = async (req, res) =>
                 quality: 'highestaudio',
                 requestOptions: {
                     headers: {
-                        Cookie: process.env.YT_COOKIE
+                        Cookie: process.env.YT_COOKIE,
+                        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
                     }
                 }
             })
